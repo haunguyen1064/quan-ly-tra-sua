6f74eae77f99c887b16b955447b566c2856c5ab2
Hau Nguyen <haunguyen1064@gmail.com>
2024-10-27 16:08:23 +0700

    add scroll for history view

--- src/main/java/views/employee/HistoryLoginView.java
+++ src/main/java/views/employee/HistoryLoginView.java
@@ -23,19 +23,23 @@ public class HistoryLoginView extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-
         panelContent1 = new javax.swing.JPanel();
+        JScrollPane scrollPane = new JScrollPane(panelContent1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Lịch sử đăng nhập");
-        setMaximumSize(new java.awt.Dimension(410, 600));
-        setMinimumSize(new java.awt.Dimension(410, 300));
-        setPreferredSize(new java.awt.Dimension(410, 300));
+        setMaximumSize(new java.awt.Dimension(450, 600));
+        setMinimumSize(new java.awt.Dimension(450, 300));
+        setPreferredSize(new java.awt.Dimension(450, 300));
         getContentPane().setLayout(new java.awt.CardLayout());
 
-        panelContent1.setMaximumSize(new java.awt.Dimension(403, 298));
-        panelContent1.setMinimumSize(new java.awt.Dimension(403, 298));
-        getContentPane().add(panelContent1, "card3");
+        panelContent1.setLayout(new javax.swing.BoxLayout(panelContent1, javax.swing.BoxLayout.Y_AXIS));
+        panelContent1.setMaximumSize(new java.awt.Dimension(450, 298));
+        panelContent1.setMinimumSize(new java.awt.Dimension(450, 298));
+        scrollPane.setMaximumSize(new java.awt.Dimension(450, 298));
+        scrollPane.setMinimumSize(new java.awt.Dimension(450, 298));
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        getContentPane().add(scrollPane, "card3");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
